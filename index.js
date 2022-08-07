cea37d57218bcba8238e166fd3ceff9eb584d3b7
Elna JP <eriosdd@gmail.com>
2022-08-07 11:21:06 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name,email
+      }),
+    })
+      .then((response) => response.json())
+      .then(data => renderId(data))
+      .catch((error)  => {
+        document.body.innerHTML = error;
+      });
+  }
+  const renderId =dtata => {
+     document.body.innerHTML = data.id;
+  }
+
+  
+
+
+  
